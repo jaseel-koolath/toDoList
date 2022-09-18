d0bcc12a932f98d2667ca25bdfda737778560b5a
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2022-09-18 15:30:00 +0530

    Added Proc file and port for heroku

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.get("/about", function(req, res) {
     res.render("about");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("The server is listening on port 3000");
 });
